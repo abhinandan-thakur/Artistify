2bebe292394651187edb2a9e2691ea0df78b0c45
abhinandan-thakur <distractionFree.C.C.Parent@gmail.com>
2026-05-23 06:33:26 +0000

    when this is going to end

--- internal/service/album_service.go
+++ internal/service/album_service.go
@@ -7,8 +7,8 @@ import (
 	"encoding/json"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
-	"time"
 	"log"
+	"time"
 )
 
 func GetAlbums(pool *pgxpool.Pool, rdb *redis.Client) ([]models.Albums, string, error) {
