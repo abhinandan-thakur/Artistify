49f1dc05f73a8ba973b11d11c2dd92727d2f81b2
abhinandan-thakur <distractionFree.C.C.Parent@gmail.com>
2026-05-23 06:31:04 +0000

    fix linter in ci

--- internal/service/album_service.go
+++ internal/service/album_service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 	"time"
+	"log"
 )
 
 func GetAlbums(pool *pgxpool.Pool, rdb *redis.Client) ([]models.Albums, string, error) {
